fed66894087550e45267a51dc2ff5d087cf51b78
AkhilDeep Shrivastava <akhildeep.shrivastava@gmail.com>
2020-11-26 20:08:09 -0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Shweta Shrivastava on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
